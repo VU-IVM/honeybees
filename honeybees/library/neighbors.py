f8c6bc7b6002662a9af13903f36fee2c2cb94740
Jens de Bruijn <j.a.debruijn@outlook.com>
2022-09-27 15:09:24 +0200

    include search_target_ids parameter

--- honeybees/library/neighbors.py
+++ honeybees/library/neighbors.py
@@ -15,13 +15,13 @@ def find_neighbor_search_count_index(search_counts, search_count_index, r):
 
 @nb.njit(parallel=True, locals={"r": nb.uint32}, cache=True)
 def find_neighbors_numba(
-    search_unique_hashcodes,
-    find_unique_hashcodes,
-    search_hashcodes_sort_indices,
+    search_target_unique_hashcodes,
+    search_source_unique_hashcodes,
+    search_target_hashcodes_sort_indices,
     cumulative_count_per_search_hashcode,
-    find_cumulative_counts_per_hashcode,
-    find_search_indices,
-    reverse_find_hashcodes_sort_indices,
+    search_source_cumulative_counts_per_hashcode,
+    map_search_source_to_target,
+    reverse_search_source_hashcodes_sort_indices,
     n_neighbor,
     radius,
     bits,
@@ -33,13 +33,13 @@ def find_neighbors_numba(
     grid
 ):
     nanvalue = np.iinfo(dtype).max # use the maximum value of uint as nanvalue
-    neighbors = np.full((find_search_indices.size, n_neighbor), nanvalue, dtype=dtype)
-    n_unique_hashcodes = search_unique_hashcodes.size
-    for u in nb.prange(find_unique_hashcodes.size):
-        find_hashcode = find_unique_hashcodes[u]
-        x, y = geohash.decode(find_hashcode, bits, minx=minx, maxx=maxx, miny=miny, maxy=maxy)
+    neighbors = np.full((map_search_source_to_target.size, n_neighbor), nanvalue, dtype=dtype)
+    n_unique_hashcodes = search_target_unique_hashcodes.size
+    for u in nb.prange(search_source_unique_hashcodes.size):
+        search_source_hashcode = search_source_unique_hashcodes[u]
+        x, y = geohash.decode(search_source_hashcode, bits, minx=minx, maxx=maxx, miny=miny, maxy=maxy)
         shifts = geohash.get_shifts(x, y, radius, bits, minx, maxx, miny, maxy, grid)
-        neighbor_geohashes = geohash.shift_multiple(find_hashcode, bits, shifts)
+        neighbor_geohashes = geohash.shift_multiple(search_source_hashcode, bits, shifts)
         neighbor_geohashes = np.sort(neighbor_geohashes)
         search_counts = np.zeros((neighbor_geohashes.size, 3), dtype=dtype)
 
@@ -50,21 +50,21 @@ def find_neighbors_numba(
             if neighbor_geohash == previous_neighbor_geohash + 1:
                 if index == n_unique_hashcodes:
                     break
-                if search_unique_hashcodes[index] == neighbor_geohash:
+                if search_target_unique_hashcodes[index] == neighbor_geohash:
                     search_counts[search_count_index, 1] += (cumulative_count_per_search_hashcode[index+1] - cumulative_count_per_search_hashcode[index])
                     index += 1
             else:
                 if search_counts[search_count_index, 1] != 0:
                     search_count_index += 1
-                index = np.searchsorted(search_unique_hashcodes, neighbor_geohash)
+                index = np.searchsorted(search_target_unique_hashcodes, neighbor_geohash)
                 if index == n_unique_hashcodes:
                     break
                 search_counts[search_count_index, 0] = cumulative_count_per_search_hashcode[index]
-                if search_unique_hashcodes[index] == neighbor_geohash:
+                if search_target_unique_hashcodes[index] == neighbor_geohash:
                     search_counts[search_count_index, 1] += (cumulative_count_per_search_hashcode[index+1] - cumulative_count_per_search_hashcode[index])
                     index += 1
 
-            if neighbor_geohash == find_hashcode:
+            if neighbor_geohash == search_source_hashcode:
                 self_index = search_count_index
             previous_neighbor_geohash = neighbor_geohash
 
@@ -84,9 +84,9 @@ def find_neighbors_numba(
             # The chances of a collision increase at the same rate that the number of values decreases,
             # so the probability of any one number being in the result is not skewed, or biased.
             # from: https://codereview.stackexchange.com/questions/61338/generate-random-numbers-without-repetitions
-            for neighbor_index_sorted in range(find_cumulative_counts_per_hashcode[u], find_cumulative_counts_per_hashcode[u+1]):
-                neighbor_index = reverse_find_hashcodes_sort_indices[neighbor_index_sorted]
-                find_agent_index = find_search_indices[neighbor_index_sorted] - search_counts[self_index, 0] + search_counts[:self_index, 1].sum()
+            for neighbor_index_sorted in range(search_source_cumulative_counts_per_hashcode[u], search_source_cumulative_counts_per_hashcode[u+1]):
+                neighbor_index = reverse_search_source_hashcodes_sort_indices[neighbor_index_sorted]
+                search_target_agent_index = map_search_source_to_target[neighbor_index_sorted] - search_counts[self_index, 0] + search_counts[:self_index, 1].sum()
                 J = n_neighbors - n_neighbor
 
                 if n_neighbor > 20:
@@ -94,7 +94,7 @@ def find_neighbors_numba(
                 
                 for i in range(n_neighbor):
                     r = np.random.randint(0, J+1)
-                    if r >= find_agent_index:  # ensure we do not pick the agent itself and include the last agent
+                    if r >= search_target_agent_index:  # ensure we do not pick the agent itself and include the last agent
                         r += 1
                     
                     neighbor_search_counts_index = find_neighbor_search_count_index(search_counts, search_count_index, r)
@@ -105,7 +105,7 @@ def find_neighbors_numba(
                     if n_neighbor > 20:
                         if neighbor in selected_agents:
                             r = int(J)
-                            if r >= find_agent_index:   # ensure we do not pick the agent itself and include the last agent
+                            if r >= search_target_agent_index:   # ensure we do not pick the agent itself and include the last agent
                                 r += 1
                             neighbor_search_counts_index = find_neighbor_search_count_index(search_counts, search_count_index, r)
                             if neighbor_search_counts_index == 0:
@@ -117,7 +117,7 @@ def find_neighbors_numba(
                         for k in range(i):
                             if agent_neighbors[k] == neighbor:
                                 r = int(J)
-                                if r >= find_agent_index:   # ensure we do not pick the agent itself and include the last agent
+                                if r >= search_target_agent_index:   # ensure we do not pick the agent itself and include the last agent
                                     r += 1
                                 neighbor_search_counts_index = find_neighbor_search_count_index(search_counts, search_count_index, r)
                                 if neighbor_search_counts_index == 0:
@@ -130,72 +130,106 @@ def find_neighbors_numba(
                     J += 1
                 for f in range(agent_neighbors.size):
                     if agent_neighbors[f] != nanvalue:
-                        neighbors[neighbor_index, f] = search_hashcodes_sort_indices[agent_neighbors[f]]
+                        neighbors[neighbor_index, f] = search_target_hashcodes_sort_indices[agent_neighbors[f]]
                 agent_neighbors.fill(nanvalue)
         else:
-            for neighbor_index_sorted in range(find_cumulative_counts_per_hashcode[u], find_cumulative_counts_per_hashcode[u+1]):
-                neighbor_index = reverse_find_hashcodes_sort_indices[neighbor_index_sorted]
-                find_agent = find_search_indices[neighbor_index_sorted]
+            for neighbor_index_sorted in range(search_source_cumulative_counts_per_hashcode[u], search_source_cumulative_counts_per_hashcode[u+1]):
+                neighbor_index = reverse_search_source_hashcodes_sort_indices[neighbor_index_sorted]
+                search_target_agent_index = map_search_source_to_target[neighbor_index_sorted]
                 n = 0
                 for m in range(search_counts.shape[0]):
                     for o in range(search_counts[m, 1]):
                         neighbor = search_counts[m, 0] + o
-                        if neighbor != find_agent:
+                        if neighbor != search_target_agent_index:
                             agent_neighbors[n] = neighbor
                             n += 1
                 for f in range(agent_neighbors.size):
                     if agent_neighbors[f] != nanvalue:
-                        neighbors[neighbor_index, f] = search_hashcodes_sort_indices[agent_neighbors[f]]
+                        neighbors[neighbor_index, f] = search_target_hashcodes_sort_indices[agent_neighbors[f]]
                 agent_neighbors.fill(nanvalue)
     return neighbors
 
-def process_hashcodes(search_hashcodes: np.ndarray, search_ids: Union[np.ndarray, None], dtype: type) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def process_hashcodes(
+    location_hashcodes: np.ndarray,
+    search_ids: Union[np.ndarray, None],
+    search_target_ids: Union[np.ndarray, None],
+    dtype: type
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Args:
-        search_hashcodes: Geohashes of agents to search.
+        location_hashcodes: Geohashes of all agents.
         search_ids: Ids of the agents to find neighbors for. If None, search all agents.
         dtype: Datatype to use to hold indices.
 
     Returns:
-        search_unique_hashcodes: Array of unique geohashes of agents.
-        find_unique_hashcodes: Array of unique geohashes of agents to find neighbors for.
-        search_hashcodes_sort_indices: Indices that can be used to sort the `search_hashcodes`.
+        search_target_unique_hashcodes: Array of unique geohashes of agents.
+        search_source_unique_hashcodes: Array of unique geohashes of agents to find neighbors for.
+        search_target_hashcodes_sort_indices: Indices that can be used to sort the `location_hashcodes`.
         cumulative_count_per_search_hashcode: Cumulative count of the unique search hashcodes.
-        find_search_indices: 
-        find_hashcodes_sort_indices: Indices that can be used to sort the `search_hashcodes`.
+        map_search_source_to_target: 
+        search_source_hashcodes_sort_indices: Indices that can be used to sort the `location_hashcodes`.
     """
     if search_ids is None:
-        search_ids = np.arange(search_hashcodes.size, dtype=np.int64)
+        search_ids = np.arange(location_hashcodes.size, dtype=np.int64)
+    if search_target_ids is None:
+        search_target_ids = np.arange(location_hashcodes.size, dtype=np.int64)
+    
     assert isinstance(search_ids, np.ndarray)
-    search_hashcodes_sort_indices = np.argsort(search_hashcodes).astype(dtype)
-    sorted_search_hashcodes = search_hashcodes[search_hashcodes_sort_indices]
-    search_unique_hashcodes, search_unique_index = np.unique(sorted_search_hashcodes, return_index=True)
-    search_unique_index = search_unique_index.astype(dtype)
-    cumulative_count_per_search_hashcode = np.append(search_unique_index, sorted_search_hashcodes.size)
+    assert isinstance(search_target_ids, np.ndarray)
+    
+    search_target_hashcodes = location_hashcodes[search_target_ids]
+    search_target_hashcodes_sort_indices = np.argsort(search_target_hashcodes).astype(dtype)
+    sorted_search_target_hashcodes = search_target_hashcodes[search_target_hashcodes_sort_indices]
+    search_target_unique_hashcodes, search_target_unique_index = np.unique(sorted_search_target_hashcodes, return_index=True)
+    search_target_unique_index = search_target_unique_index.astype(dtype)
+    cumulative_count_per_search_target_hashcode = np.append(search_target_unique_index, sorted_search_target_hashcodes.size)
 
-    find_hashcodes = search_hashcodes[search_ids]
-    find_hashcodes_sort_indices = np.argsort(find_hashcodes).astype(dtype)
-    search_ids_sorted_by_hashcode = search_ids[find_hashcodes_sort_indices]
-    find_sorted_hashcodes = find_hashcodes[find_hashcodes_sort_indices]
-    find_unique_hashcodes, find_unique_index = np.unique(find_sorted_hashcodes, return_index=True)
-    find_unique_index = find_unique_index.astype(dtype)
-    find_cumulative_counts_per_hashcode = np.append(find_unique_index, find_sorted_hashcodes.size)
+    # search_target_hashcodes_sort_indices = np.argsort(location_hashcodes).astype(dtype)
+    # sorted_search_hashcodes = location_hashcodes[search_target_hashcodes_sort_indices]
+    # search_target_unique_hashcodes, search_unique_index = np.unique(sorted_search_hashcodes, return_index=True)
+    # search_unique_index = search_unique_index.astype(dtype)
+    # cumulative_count_per_search_hashcode = np.append(search_unique_index, sorted_search_hashcodes.size)
 
-    inverse_idx = np.empty_like(search_hashcodes_sort_indices)
-    inverse_idx[search_hashcodes_sort_indices] = np.arange(0, inverse_idx.size)
-    find_search_indices = inverse_idx[search_ids_sorted_by_hashcode]
+    search_source_hashcodes = location_hashcodes[search_ids]
+    search_source_hashcodes_sort_indices = np.argsort(search_source_hashcodes).astype(dtype)
+    search_source_ids_sorted_by_hashcode = search_ids[search_source_hashcodes_sort_indices]
+    search_source_sorted_hashcodes = search_source_hashcodes[search_source_hashcodes_sort_indices]
+    search_source_unique_hashcodes, search_source_unique_index = np.unique(search_source_sorted_hashcodes, return_index=True)
+    search_source_unique_index = search_source_unique_index.astype(dtype)
+    search_source_cumulative_counts_per_hashcode = np.append(search_source_unique_index, search_source_sorted_hashcodes.size)
+
+    inverse_idx = np.empty_like(location_hashcodes)  # should be good, we are mapping targets to sources
+    inverse_idx[np.argsort(location_hashcodes).astype(dtype)] = np.arange(0, inverse_idx.size)
+    inverse_idx[~np.isin(inverse_idx, search_target_ids)] = -1
+    map_search_source_to_target = inverse_idx[search_source_ids_sorted_by_hashcode]
+    
+    # inverse_idx = np.empty_like(search_target_hashcodes_sort_indices)
+    # inverse_idx[search_target_hashcodes_sort_indices] = np.arange(0, inverse_idx.size)
+    # map_search_source_to_target = inverse_idx[search_ids_sorted_by_hashcode]
 
     return (
-        search_unique_hashcodes,
-        find_unique_hashcodes,
-        search_hashcodes_sort_indices,
-        cumulative_count_per_search_hashcode,
-        find_cumulative_counts_per_hashcode,
-        find_search_indices,
-        find_hashcodes_sort_indices
+        search_target_unique_hashcodes,
+        search_source_unique_hashcodes,
+        search_target_hashcodes_sort_indices,
+        cumulative_count_per_search_target_hashcode,
+        search_source_cumulative_counts_per_hashcode,
+        map_search_source_to_target,
+        search_source_hashcodes_sort_indices
     )
 
-def find_neighbors(locations: np.ndarray, radius: Union[float, int], n_neighbor: int, bits: int, minx: Union[float, int] =-180, maxx: Union[float, int]=180, miny: Union[float, int]=-90, maxy: Union[float, int]=90, grid: str='longlat', search_ids: Union[None, np.ndarray]=None) -> np.ndarray:
+def find_neighbors(
+    locations: np.ndarray,
+    radius: Union[float, int],
+    n_neighbor: int,
+    bits: int,
+    minx: Union[float, int] =-180,
+    maxx: Union[float, int]=180,
+    miny: Union[float, int]=-90,
+    maxy: Union[float, int]=90,
+    grid: str='longlat',
+    search_ids: Union[None, np.ndarray]=None,
+    search_target_ids: Union[np.ndarray, None]=None,
+) -> np.ndarray:
     """Finds neighbouring agents for given agents.
 
     Args:
@@ -208,43 +242,57 @@ def find_neighbors(locations: np.ndarray, radius: Union[float, int], n_neighbor:
         miny: Minimum y-value of the entire relevant space.
         miny: Maximum y-value of the entire relevant space.
         grid: Type of grid. Can be 'longlat' or 'orthogonal'.
-        search_ids: Only agents with given indices are searched.
+        search_ids: Only search for neighbors of agent with search_ids.
+        search_target_ids: Only search neighbors with search_target_ids.
 
     Returns:
         neighbors: 2-dimensional NumPy array. The first dimension represents the results for each of the agents, the second dimension the indices of the neighbors for each of those agents. -1 represents no neighbor (possible only if less than the requested number of neighbors are available in the search radius.)
     """
     if locations.ndim == 2:
         assert locations.shape[1] == 2
-        search_hashcodes = geohash.encode_locations(locations, minx, maxx, miny, maxy)
-        search_hashcodes = geohash.reduce_precision(search_hashcodes, bits, inplace=True)
+        location_hashcodes = geohash.encode_locations(locations, minx, maxx, miny, maxy)
+        location_hashcodes = geohash.reduce_precision(location_hashcodes, bits, inplace=True)
     elif locations.ndim == 1:
-        search_hashcodes = geohash.reduce_precision(locations, bits, inplace=False)
-        assert search_hashcodes.dtype == np.int64
+        location_hashcodes = geohash.reduce_precision(locations, bits, inplace=False)
+        assert location_hashcodes.dtype == np.int64
     else:
         raise ValueError("locations must be either 2d-array with lon,lat values or 1d-array of geohashes")
 
     # test if number of locations is not larger than the maximum value an uint32 can hold. -1 because the max value is used as nan
-    if search_hashcodes.size <= np.iinfo(np.uint32).max - 1:
+    if location_hashcodes.size <= np.iinfo(np.uint32).max - 1:
         dtype = np.uint32
     else:
         dtype = np.uint64
 
-    search_unique_hashcodes, find_unique_hashcodes, search_hashcodes_sort_indices, cumulative_count_per_search_hashcode, find_cumulative_counts_per_hashcode, find_search_indices, find_hashcodes_sort_indices = process_hashcodes(search_hashcodes, search_ids, dtype)
+    (
+        search_target_unique_hashcodes,
+        search_source_unique_hashcodes,
+        search_target_hashcodes_sort_indices,
+        cumulative_count_per_search_hashcode,
+        search_source_cumulative_counts_per_hashcode,
+        map_search_source_to_target,
+        search_source_hashcodes_sort_indices
+    ) = process_hashcodes(location_hashcodes, search_ids, search_target_ids, dtype)
 
-    return find_neighbors_numba(
-        search_unique_hashcodes=search_unique_hashcodes,
-        find_unique_hashcodes=find_unique_hashcodes,
-        search_hashcodes_sort_indices=search_hashcodes_sort_indices,
+    neighbours = find_neighbors_numba(
+        search_target_unique_hashcodes=search_target_unique_hashcodes,
+        search_source_unique_hashcodes=search_source_unique_hashcodes,
+        search_target_hashcodes_sort_indices=search_target_hashcodes_sort_indices,
         cumulative_count_per_search_hashcode=cumulative_count_per_search_hashcode,
-        find_cumulative_counts_per_hashcode=find_cumulative_counts_per_hashcode,
-        find_search_indices=find_search_indices,
-        reverse_find_hashcodes_sort_indices=find_hashcodes_sort_indices,
+        search_source_cumulative_counts_per_hashcode=search_source_cumulative_counts_per_hashcode,
+        map_search_source_to_target=map_search_source_to_target,
+        reverse_search_source_hashcodes_sort_indices=search_source_hashcodes_sort_indices,
         n_neighbor=n_neighbor,
         radius=radius,
         bits=bits,
         dtype=dtype,
         minx=minx, maxx=maxx, miny=miny, maxy=maxy, grid=grid
     )
+
+    if search_target_ids is not None:
+        neighbours = search_target_ids[neighbours]
+
+    return neighbours
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
