c4b4892f70aa75d8ec654fc64d81a5ec32f91a8c
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-01-05 15:58:34 +0100

    Run without visual interface by default. Instead specify --GUI when visual interface should be used.

--- honeybees/argparse.py
+++ honeybees/argparse.py
@@ -3,8 +3,8 @@ import argparse
 from honeybees import __name__
 
 parser = argparse.ArgumentParser(description=f"{__name__.title()} is a package for high-speed geographical agent-based modelling in Python")
-parser.add_argument('--headless', dest='headless', action='store_true', help=f"The model can be run with or without a visual interface. The visual interface is useful to display the results in real-time while the model is running and to better understand what is going on. You can simply start or stop the model with the click of a buttion, or advance the model by an `x` number of timesteps. However, the visual interface is much slower than running the model without it.")
-parser.set_defaults(headless=False)
+parser.add_argument('--GUI', dest='GUI', action='store_true', help=f"The model can be run with or without a visual interface. The visual interface is useful to display the results in real-time while the model is running and to better understand what is going on. You can simply start or stop the model with the click of a buttion, or advance the model by an `x` number of timesteps. However, the visual interface is much slower than running the model without it.")
+parser.set_defaults(GUI=False)
 parser.add_argument('--no-browser', dest='browser', action='store_false', help=f"Do not open browser when running the model. This option is, for example, useful when running the model on a server, and you would like to remotely access the model.")
 parser.set_defaults(browser=True)
 default_port = 8521
