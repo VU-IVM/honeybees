7b5c28a6c59d90a14655f3a5847d18f29f0cf813
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-08-16 12:04:45 +0200

    test release

--- tests/run.bat
+++
@@ -1,9 +0,0 @@
-echo off
-cd /D "%~dp0"
-if not exist "benchmarks" mkdir benchmarks
-
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
-set datetime=%datetime:~0,8%-%datetime:~8,6%
-
-pytest -s --plots plots --benchmark-json=benchmarks/benchmark_%datetime%.json --benchmark-warmup=on --benchmark-max-time=3600 --extended
-@REM python plot_benchmarks.py --file benchmark_%datetime%.json
--- tests/run.sh
+++
@@ -1,4 +0,0 @@
-cd "$(dirname "$(realpath "$0")")";
-mkdir -p benchmarks
-pytest -s --plots plots --benchmark-json=benchmarks/benchmark.json --benchmark-warmup=on --benchmark-max-time=1000000000 --extended
-
